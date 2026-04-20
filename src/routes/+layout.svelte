d64d65a81ccf49f2db2c5945f2306d567b785be9
Tihomir971 <tihomir.jev@outlook.com>
2026-04-20 18:28:30 +0200

    Switch default font to Nunito Sans

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { LocaleProvider } from '@ark-ui/svelte/locale';
 	import './layout.css';
-	import '@fontsource-variable/wix-madefor-text/wght.css';
+	// Supports weights 200-900
+	import '@fontsource-variable/nunito-sans/wght.css';
 
 	const { children } = $props();
 </script>
